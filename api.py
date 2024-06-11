f6bef23dc88d0b3d161ad6a6e5e56a5c693c38cf
Mouzone <sunnyliu010@gmail.com>
2024-06-10 21:36:36 -0400

    Rename tables to match new supreme db names

--- api.py
+++ api.py
@@ -3,24 +3,24 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 import os
 
 
-class Item(SQLModel, table=True):
+class Items(SQLModel, table=True):
     item_id: int = Field(default=None, primary_key=True)
     product: str
     price: float
 
 
-class Variant(SQLModel, table=True):
+class Variants(SQLModel, table=True):
     variant_id: int = Field(default=None, primary_key=True)
-    item_id: int = Field(foreign_key="item.item_id")
+    item_id: int = Field(foreign_key="items.item_id")
     variant: str
     img_link: str
     url: str
 
 
-class Size(SQLModel, table=True):
+class Sizes(SQLModel, table=True):
     size_id: int = Field(default=None, primary_key=True)
-    item_id: int = Field(foreign_key="item.item_id")
-    variant_id: int = Field(foreign_key="variant.variant_id")
+    item_id: int = Field(foreign_key="items.item_id")
+    variant_id: int = Field(foreign_key="variants.variant_id")
     size: str
 
 
